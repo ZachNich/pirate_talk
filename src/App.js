afa0ec00cd4698a724b86297ae29e5f18d49fd6b
Zach Nicholson <raiste.doctow@gmail.com>
2020-07-18 14:05:02 -0500

    translations data and translate function finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import translations from './Translations';
+import translate from './translate';
 
 function App() {
   return (
-    0
+    <>
+      0
+    </>
   );
 }
 
