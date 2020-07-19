d6fa7455bed2a5d4f6487854b9bba97f8f2660d0
Zach Nicholson <raiste.doctow@gmail.com>
2020-07-18 20:46:11 -0500

    added refresh suggest btn, fixed empty suggestion error

--- src/suggest.js
+++ src/suggest.js
@@ -1,13 +1,13 @@
 const suggest = translations => {
     const keys = []
     const suggestions = []
-
     for (let i = 0; i < translations.length; i++) {
         keys.push(Object.keys(translations[i]).toString())
+        console.log(Object.keys(translations[i]).toString())
     }
 
     for (let i = 0; i < 4; i++) {
-        suggestions.push(keys[Math.floor(Math.random() * translations.length + 1)])
+        suggestions.push(keys[Math.floor(Math.random() * translations.length)])
     }
 
     return suggestions;
